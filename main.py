d24deee1830a6e70def6d305494a86fbf0b9b833
Julio Maniratunga <julio73@users.noreply.github.com>
2020-05-15 16:59:18 +0300

    First helper

---
+++ main.py
@@ -0,0 +1,25 @@
+
+# Find block of time within calendars
+# Times:
+# T: 10     11      12      13      14      15
+# A:    | ---------         ---     -----   |
+# B:  | ----        ----    ----        ----|
+
+# Constraints:
+
+# - Within block of calendar time
+#   - Find higher bound for earliest time
+#   - Find lower bound for highest time
+def getTimeBound(bound_tuples):
+    if len(bound_tuples) == 0:
+        return None
+    time_bound = bound_tuples[0]
+    for bound in bound_tuples[1:]:
+        if time_bound[0] < bound[0]:
+            time_bound[0] = bound[0]
+        if time_bound[1] > bound[1]:
+            time_bound[1] = bound[1]
+    return time_bound
+
+if __name__ == "__main__":
+    pass
