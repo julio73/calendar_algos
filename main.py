28c5ee1fca73b00bd16c4d9fc8c5b2125fc1a1fc
Julio Maniratunga <julio73@users.noreply.github.com>
2020-05-15 23:16:39 +0300

    Fix on first function for bounds that don't meet

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def getTimeBound(bound_tuples):
             time_bound[0] = bound[0]
         if time_bound[1] > bound[1]:
             time_bound[1] = bound[1]
+    if (time_bound[0] > time_bound[1]) or (time_bound[0] > time_bound[1]):
+        return None
     return time_bound
 
 if __name__ == "__main__":
