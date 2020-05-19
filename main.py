185b562c7deee4fe3a2553ce6bd535e8f2f2acbf
Julio Maniratunga <julio73@users.noreply.github.com>
2020-05-20 01:28:56 +0300

    Determine containance of duration. Little clean up

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def getTimeBound(bound_tuples):
         return None
     return time_bound
 
+
 # - Time passed in military hour as a string
 #   - Convert it to integer for comparison
 #   - Convert back for output
@@ -30,19 +31,24 @@ def getTimeParts(time_string):
     hour, minute = map(int, time_string.split(':'))
     return (hour, minute)
 
+
 def getTimeDifference(time_string_a, time_string_b):
     if time_string_a == time_string_b:
         return (0, 0)
     else:
         hour_a, minute_a = getTimeParts(time_string_a)
         hour_b, minute_b = getTimeParts(time_string_b)
-        
         time_diff = abs((hour_b * 60 + minute_b) - (hour_a * 60 + minute_a))
         diff_mins = time_diff % 60
         diff_hours = int((time_diff - diff_mins) / 60)
-
         return (diff_hours, diff_mins)
 
+# - Time block should be within given bound
+#   - Each bound is inclusive
+def canContainDuration(time_a, time_b, duration):
+    bound_hours, bound_minutes = getTimeDifference(time_a, time_b)
+    duration_hours, duration_minutes = getTimeParts(duration)
+    return (bound_hours * 60 + bound_minutes) >= (duration_hours * 60 + duration_minutes)
 
 
 
